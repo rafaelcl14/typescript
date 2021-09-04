e3f335d5f0d963db007d82d97532b7c7c88560ad
rafaelcl14 <rafaelcl1497@gmail.com>
2021-09-04 09:01:51 -0400

    Initial Commit

---
+++ app/models/negociacao.ts
@@ -0,0 +1,27 @@
+export class Negociacao {
+  private _data;
+  private _quantidade;
+  private _valor;
+
+  constructor(data : Date, quantidade: number, valor: number) {
+    this._data = data;
+    this._quantidade = quantidade;
+    this._valor = valor;
+  }
+
+  get data(): Date{
+    return this._data;
+  }
+
+  get quantidade(): number {
+    return this._quantidade;
+  }
+
+  get valor(): number{
+    return this._valor;
+  }
+
+  get volume(): number {
+    return this._quantidade * this._valor;
+  }
+}
